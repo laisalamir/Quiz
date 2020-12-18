84a46d5c2953103bc64fcf559dd99b310ae1727f
laisalamir <lmslamir@gmail.com>
2020-12-18 18:22:17 +0000

    Upup

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,13 +1,43 @@
-function terminei() {
+function prox1() {
+    document.getElementById("button1").style.display = "none";
+    document.getElementById("quiz1").style.visibility = "visible";
+    if (quiz1?.value == "0") {
+        alert("Escolha umas das opções !!")
+    }
+}
 
+function prox2() {
+    document.getElementById("quiz1").style.display = "none";
+    document.getElementById("quiz2").style.visibility = "visible";
+    if (quiz2?.value == "0") {
+        alert("Escolha umas das opções !!")
+    }
+}
+
+function prox3() {
+    document.getElementById("quiz2").style.display = "none";
+    document.getElementById("quiz3").style.visibility = "visible";
+}
+
+function prox4() {
+    document.getElementById("quiz3").style.display = "none";
+    document.getElementById("quiz4").style.visibility = "visible";
+}
+
+function prox5() {
+    document.getElementById("quiz4").style.display = "none";
+    document.getElementById("quiz5").style.visibility = "visible";
+}
+
+function terminei() {
     let question1 = [...document.getElementsByName('question1')].filter(el => el.checked)[0]
     let question2 = [...document.getElementsByName('question2')].filter(el => el.checked)[0]
     let question3 = [...document.getElementsByName('question3')].filter(el => el.checked)[0]
     let question4 = [...document.getElementsByName('question4')].filter(el => el.checked)[0]
     let question5 = [...document.getElementsByName('question5')].filter(el => el.checked)[0]
 
-    correct = 0;
-    wrong = 0;
+    let = correct = 0;
+    let = wrong = 0;
 
     if (question1?.value === "61") {
         console.log('Correct Answer')
@@ -67,11 +97,12 @@ function terminei() {
     if (correct == 5) {
         range = 0;
     }
-
+    document.getElementById("quiz5").style.display = "none";
     document.getElementById("result").style.visibility = "visible";
     document.getElementById("certa").innerHTML += `<p>${correct} correct.</p>`;
     document.getElementById("errada").innerHTML += `<p>${wrong} wrong.</p>`;
     document.getElementById("msg").innerHTML = messages[range];
-    
+    document.getElementById("gif");
+
 }
 
